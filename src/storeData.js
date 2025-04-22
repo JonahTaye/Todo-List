aaf26cbefc69b0436cdf1aafe9bd20188b194c93
jonah <jonah.softwaredev@gmail.com>
2025-04-22 11:26:34 +0300

    feat: add changeStatus function and update the other functions

--- src/storeData.js
+++ src/storeData.js
@@ -1,26 +1,39 @@
 import { storage } from "./storageManager"
 import { Todo, Group } from "./todo"
-import { displayGroup } from "./displayModule"
+import { displayCard, displayGroup } from "./displayModule"
 
 export function addTask(form, values) {
     const formwithId = 6
-
+    
     if (form.length < formwithId) {
         const id = "none"
         values.unshift(id)
-        const task = new Todo(...values)
-        console.log(task)
         
+        const task = new Todo(...values)
         storage.setTask(task)
     } else {
         const task = new Todo(...values)
-        console.log(task)
+        storage.updateTask(task)
     }
 }
 
-export function addGroup(values) {
-    const group = new Group(...values)
+export function addGroup(form, values) {
+    const formwithId = 2
     
-    storage.setGroup(group)
+    if(form.length < formwithId) {
+        const id = "none"
+        values.unshift(id)
+        const group = new Group(...values)
+        storage.setGroup(group)
+    } else {
+        const newGroup = new Group(...values)
+        storage.updateGroup(newGroup)
+    }
     displayGroup()
+}
+
+export function changeStatus(id) {
+    let task = storage.getTask(parseInt(id))
+    task.status = true
+    storage.updateTask(task)
 }
