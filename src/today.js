35a2410cb1554944ba2d6d444e9aa1d4f05982d5
jonah <jonah.softwaredev@gmail.com>
2025-04-22 14:29:41 +0300

    feat: add functions to display cards for side pane options

--- src/today.js
+++ src/today.js
@@ -1,12 +1,130 @@
 import { displayCard } from "./displayModule"
-import { StorageManager } from "./storageManager"
-import { openForm } from "./forms"
+import { storage } from "./storageManager"
+
+const header = document.querySelector(".content > .title")
+const formTask = document.querySelector("#task-form")
 
 export const todayDisplay = function() {
-    const header = document.querySelector(".content > .title")
     header.textContent = "Today"
-    const card = document.querySelector(".card")
-    const container = document.querySelector(".card-container")
+    formTask.className = ""
+    formTask.classList.add("today")
 
-    displayCard()
+    
+    todayDisplayCard()
+}
+
+export function displayUpcoming() {
+    header.textContent = "Upcoming"
+    formTask.className = ""
+    formTask.classList.add("upcoming")
+
+    displayUpcomingCard()
+}
+
+export function displayCompleted() {
+    header.textContent = "Completed"
+    formTask.className = ""
+    formTask.classList.add("completed")
+
+    displayCompletedCard()
+}
+
+export function displayAll() {
+    header.textContent = "All Tasks"
+    formTask.className = ""
+    formTask.classList.add("all-tasks")
+
+    
+    displayCardAll()
+}
+
+export function todayDisplayCard() {
+    const container = document.querySelector(".card-container")
+    const groups = storage.getAllGroups()
+    const tasks = []
+    let today = new Date
+    today = today.toISOString().split("T")[0]
+    
+    container.innerHTML = ""
+    for(let group in groups) {
+        for (let task of groups[group].tasks) {
+            if(task.dueDate === today && task.status != true) {
+                tasks.push(task)
+            }
+        }
+    }
+    tasks.sort((task1, task2) => task1.priority - task2.priority)
+    displayCard(tasks)
+}
+
+export function displayCardAll() {
+    const defaultId = 1000000000001
+    const container = document.querySelector(".card-container")
+    const defaultGroup = storage.getGroup(defaultId)
+    const tasks = []
+
+    container.innerHTML = ""
+    for (let task of defaultGroup.tasks) {
+        if(task.status != true) tasks.push(task)
+    }
+
+    tasks.sort((task1, task2) => task1.priority - task2.priority)
+    displayCard(tasks)
+
+}
+
+export function displayCompletedCard() {
+    const container = document.querySelector(".card-container")
+    const groups = storage.getAllGroups()
+    const completed = []
+
+    container.innerHTML = ""
+    for(let group in groups) {
+        for (let task of groups[group].tasks) {
+            if(task.status === true) {
+                completed.push(task)
+            }
+        }
+    }
+
+    completed.sort((task1, task2) => task1.priority - task2.priority)
+    displayCard(completed)
+}
+
+export function displayUpcomingCard() {
+    const container = document.querySelector(".card-container")
+    const groups = storage.getAllGroups()
+    const tasks = []
+    let today = new Date()
+
+    
+    container.innerHTML = ""
+    for(let group in groups) {
+        for (let task of groups[group].tasks) {
+            let date = new Date(task.dueDate)
+            
+            if(date > today && task.status != true) {
+                tasks.push(task)
+            }
+        }
+    }
+    tasks.sort((task1, task2) => task1.priority - task2.priority)
+    displayCard(tasks)
+}
+
+export function displayGroupCard(id) {
+    formTask.className = ""
+    formTask.classList.add("upcoming")
+    console.log("Group id", id)
+    const container = document.querySelector(".card-container")
+    const groupItems = storage.getGroup(parseInt(id))
+    let tasks = []
+
+    header.textContent = groupItems.name
+    tasks = groupItems.tasks
+    
+    container.innerHTML = ""
+    tasks.sort((task1, task2) => task1.priority - task2.priority)
+    
+    displayCard(tasks)
 }
